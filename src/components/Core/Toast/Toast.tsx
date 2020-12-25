535f560a7412630ca3709c9cf689521e0c458acd
jayce <phathoang@incognito.org>
2020-12-25 17:32:19 +0700

    APP-473

--- src/components/Core/Toast/Toast.tsx
+++ src/components/Core/Toast/Toast.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
-import { MdError, MdWarning, MdInfo, MdClose } from 'react-icons/md';
+import {
+    // MdError,
+    //  MdWarning,
+    //  MdInfo,
+    MdClose,
+} from 'react-icons/md';
 import { COLORS } from 'src/styles';
 import { toastSelector } from './Toast.selector';
-import { TOAST_CONFIGS, TOAST_CONFIGS_CLASSNAME } from './Toast.reducer';
+import {
+    //  TOAST_CONFIGS,
+    TOAST_CONFIGS_CLASSNAME,
+} from './Toast.reducer';
 import { actionToggleToast } from './Toast.actions';
 
 const Styled = styled.div`
@@ -34,7 +42,7 @@ const Styled = styled.div`
         color: ${COLORS.orange};
     }
     p.fs-small {
-        margin-top: 10px;
+        /* margin-top: 10px; */
     }
     .close-icon {
         position: absolute;
@@ -51,18 +59,18 @@ const Toast = () => {
     const dispatch = useDispatch();
     const { value, type, toggle } = useSelector(toastSelector);
     const className = `${TOAST_CONFIGS_CLASSNAME[String(type)]} ${toggle ? 'show' : ''}`;
-    const getIcons = () => {
-        switch (type) {
-            case TOAST_CONFIGS.error:
-                return <MdError size={16} />;
-            case TOAST_CONFIGS.success:
-                return <MdInfo size={16} />;
-            case TOAST_CONFIGS.warning:
-                return <MdWarning size={16} />;
-            default:
-                return <MdInfo size={16} />;
-        }
-    };
+    // const getIcons = () => {
+    //     switch (type) {
+    //         case TOAST_CONFIGS.error:
+    //             return <MdError size={16} />;
+    //         case TOAST_CONFIGS.success:
+    //             return <MdInfo size={16} />;
+    //         case TOAST_CONFIGS.warning:
+    //             return <MdWarning size={16} />;
+    //         default:
+    //             return <MdInfo size={16} />;
+    //     }
+    // };
     const handleCloseToast = () => dispatch(actionToggleToast({ toggle: false }));
 
     React.useEffect(() => {
@@ -78,7 +86,7 @@ const Toast = () => {
     return (
         <Styled className={className}>
             <div className="scroll-view">
-                {getIcons()}
+                {/* {getIcons()} */}
                 <p className="fs-small fw-small">{value}</p>
             </div>
             <MdClose onClick={handleCloseToast} size={18} className="close-icon" />
