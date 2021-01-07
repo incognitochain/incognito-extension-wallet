c344b4bd7d665c80ea7d98c1ab6e300aba98e58d
jayce <phathoang@incognito.org>
2021-01-07 11:59:36 +0700

    APP-508

--- src/module/Token/features/Detail/Detail.styled.ts
+++ src/module/Token/features/Detail/Detail.styled.ts
@@ -8,35 +8,21 @@ export const Styled = styled.div`
         display: flex;
         flex-direction: column;
     }
-    .token-balance .amount-token {
-        margin-bottom: 10px;
-    }
     .token-balance .balance-token {
         color: ${(props: IGlobalStyle) => props.theme.text};
     }
     .btn-container {
         margin: 30px 0;
     }
-    .hook-container {
-        display: flex;
-        align-items: flex-start;
-    }
-    .hook-container > .icon {
-        width: 18px;
-        height: 18px;
-        margin-top: 2px;
-        margin-left: unset;
-    }
-    .hook-container {
-        height: 43px;
-        > :first-child {
-            margin-right: 5px;
-        }
-    }
-    .hook-container .amount-token {
+    .amount-token {
         max-width: 250px;
+        height: 40px;
+        line-height: 40px;
     }
     .header-title {
         max-width: 200px;
+    }
+    .balance-token {
+        margin-top: 5px;
     }
 `;
