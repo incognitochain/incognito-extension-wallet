296675098adfdca44de6a31d4eca5a401b586e50
jayce <phathoang@incognito.org>
2021-01-20 15:39:23 +0700

    Switch account load balance

--- src/module/Account/Account.actions.ts
+++ src/module/Account/Account.actions.ts
@@ -5,9 +5,10 @@ import { Dispatch } from 'redux';
 import { IRootState } from 'src/redux/interface';
 import { actionHandleLoadWallet, actionSaveWallet, actionUpdateWallet, IWalletReducer } from 'src/module/Wallet';
 import { walletDataSelector, walletSelector } from 'src/module/Wallet/Wallet.selector';
-import { actionFollowDefaultToken } from 'src/module/Token';
+import { actionFollowDefaultToken, actionGetPrivacyTokensBalance } from 'src/module/Token';
 import { actionFreeHistory } from 'src/module/History';
 import { cachePromise } from 'src/services';
+import { batch } from 'react-redux';
 import {
     ACTION_FETCHED,
     ACTION_FETCHING_CREATE_ACCOUNT,
@@ -33,75 +34,6 @@ export const actionFetched = (payload: any) => ({
     payload,
 });
 
-export const actionFetchingCreateAccount = () => ({
-    type: ACTION_FETCHING_CREATE_ACCOUNT,
-});
-
-export const actionFetchedCreateAccount = (payload: AccountInstance) => ({
-    type: ACTION_FETCHED_CREATE_ACCOUNT,
-    payload,
-});
-
-export const actionFetchCreateAccount = (accountName: string) => async (
-    dispatch: Dispatch,
-    getState: () => IRootState,
-) => {
-    try {
-        const state = getState();
-        const wallet: WalletInstance = walletDataSelector(state);
-        const create = createAccountSelector(state);
-        if (create) {
-            return;
-        }
-        await dispatch(actionFetchingCreateAccount());
-        const account = await wallet.masterAccount.addAccount(accountName);
-        if (!account) {
-            throw new Error(`Can't not create account`);
-        }
-        await dispatch(actionFetchedCreateAccount(account));
-        await actionFollowDefaultToken(account)(dispatch, getState);
-        await wallet.update();
-    } catch (error) {
-        throw error;
-    } finally {
-        actionSaveWallet()(dispatch, getState);
-    }
-};
-
-export const actionFetchingImportAccount = () => ({
-    type: ACTION_FETCHING_IMPORT_ACCOUNT,
-});
-
-export const actionFetchedImportAccount = (payload: AccountInstance) => ({
-    type: ACTION_FETCHED_IMPORT_ACCOUNT,
-    payload,
-});
-
-export const actionFetchImportAccount = (accountName: string, privateKey: string) => async (
-    dispatch: Dispatch,
-    getState: () => IRootState,
-) => {
-    try {
-        const state = getState();
-        const wallet: WalletInstance = walletDataSelector(state);
-        const importing = importAccountSelector(state);
-        if (importing) {
-            return;
-        }
-        await dispatch(actionFetchingImportAccount());
-        const account = await wallet.masterAccount.importAccount(accountName, privateKey);
-        if (!account) {
-            throw new Error(`Can't not create account`);
-        }
-        await dispatch(actionFetchedImportAccount(account));
-        await actionFollowDefaultToken(account)(dispatch, getState);
-    } catch (error) {
-        throw error;
-    } finally {
-        await actionSaveWallet()(dispatch, getState);
-    }
-};
-
 export const actionSelectAccount = (payload: string) => ({
     type: ACTION_SELECT_ACCOUNT,
     payload,
@@ -112,37 +44,7 @@ export const actionSetListAccount = (payload: AccountInstance[]) => ({
     payload,
 });
 
-export const actionSwitchAccountFetching = () => ({
-    type: ACTION_SWITCH_ACCOUNT_FETCHING,
-});
-
-export const actionSwitchAccountFetched = () => ({
-    type: ACTION_SWITCH_ACCOUNT_FETCHED,
-});
-
-export const actionSwitchAccount = (accountName: string) => async (dispatch: Dispatch, getState: () => IRootState) => {
-    const state = getState();
-    const wallet: WalletInstance = walletDataSelector(state);
-    const defaultAccountName = defaultAccountNameSelector(state);
-    const account: AccountInstance = wallet.masterAccount.getAccountByName(accountName);
-    try {
-        if (isEqual(account?.name, defaultAccountName)) {
-            return account;
-        }
-        await dispatch(actionSwitchAccountFetching());
-        if (!account) {
-            throw new Error(`Account not found!`);
-        }
-        await dispatch(actionSelectAccount(account.name));
-        await actionHandleLoadWallet()(dispatch, getState);
-    } catch (error) {
-        throw error;
-    } finally {
-        await dispatch(actionFreeHistory());
-        await dispatch(actionSwitchAccountFetched());
-    }
-    return account;
-};
+// get account balance
 
 export const actionGetAccountBalanceFetching = (payload: { accountName: string }) => ({
     type: ACTION_GET_ACCOUNT_BALANCE_FETCHING,
@@ -189,6 +91,119 @@ export const actionGetAccountBalance = (defaultAccount?: AccountInstance | undef
     }
 };
 
+// switch account
+
+export const actionSwitchAccountFetching = () => ({
+    type: ACTION_SWITCH_ACCOUNT_FETCHING,
+});
+
+export const actionSwitchAccountFetched = () => ({
+    type: ACTION_SWITCH_ACCOUNT_FETCHED,
+});
+
+export const actionSwitchAccount = (accountName: string) => async (dispatch: Dispatch, getState: () => IRootState) => {
+    const state = getState();
+    const wallet: WalletInstance = walletDataSelector(state);
+    const defaultAccountName = defaultAccountNameSelector(state);
+    const account: AccountInstance = wallet.masterAccount.getAccountByName(accountName);
+    try {
+        if (isEqual(account?.name, defaultAccountName)) {
+            return account;
+        }
+        await dispatch(actionSwitchAccountFetching());
+        if (!account) {
+            throw new Error(`Account not found!`);
+        }
+        await dispatch(actionSelectAccount(account.name));
+        batch(() => {
+            actionHandleLoadWallet()(dispatch, getState);
+            actionGetAccountBalance(account)(dispatch, getState);
+            actionGetPrivacyTokensBalance(account)(dispatch, getState);
+            dispatch(actionFreeHistory());
+        });
+    } catch (error) {
+        throw error;
+    } finally {
+        await dispatch(actionSwitchAccountFetched());
+    }
+    return account;
+};
+
+// create account
+
+export const actionFetchingCreateAccount = () => ({
+    type: ACTION_FETCHING_CREATE_ACCOUNT,
+});
+
+export const actionFetchedCreateAccount = (payload: AccountInstance) => ({
+    type: ACTION_FETCHED_CREATE_ACCOUNT,
+    payload,
+});
+
+export const actionFetchCreateAccount = (accountName: string) => async (
+    dispatch: Dispatch,
+    getState: () => IRootState,
+) => {
+    try {
+        const state = getState();
+        const wallet: WalletInstance = walletDataSelector(state);
+        const create = createAccountSelector(state);
+        if (create) {
+            return;
+        }
+        await dispatch(actionFetchingCreateAccount());
+        const account = await wallet.masterAccount.addAccount(accountName);
+        if (!account) {
+            throw new Error(`Can't not create account`);
+        }
+        await dispatch(actionFetchedCreateAccount(account));
+        await actionFollowDefaultToken(account)(dispatch, getState);
+        await wallet.update();
+    } catch (error) {
+        throw error;
+    } finally {
+        actionSaveWallet()(dispatch, getState);
+    }
+};
+
+// import account
+
+export const actionFetchingImportAccount = () => ({
+    type: ACTION_FETCHING_IMPORT_ACCOUNT,
+});
+
+export const actionFetchedImportAccount = (payload: AccountInstance) => ({
+    type: ACTION_FETCHED_IMPORT_ACCOUNT,
+    payload,
+});
+
+export const actionFetchImportAccount = (accountName: string, privateKey: string) => async (
+    dispatch: Dispatch,
+    getState: () => IRootState,
+) => {
+    try {
+        const state = getState();
+        const wallet: WalletInstance = walletDataSelector(state);
+        const importing = importAccountSelector(state);
+        if (importing) {
+            return;
+        }
+        await dispatch(actionFetchingImportAccount());
+        const account = await wallet.masterAccount.importAccount(accountName, privateKey);
+        if (!account) {
+            throw new Error(`Can't not create account`);
+        }
+        await dispatch(actionFetchedImportAccount(account));
+        await actionFollowDefaultToken(account)(dispatch, getState);
+    } catch (error) {
+        throw error;
+    } finally {
+        await actionSaveWallet()(dispatch, getState);
+    }
+};
+
+// remove account
+
 export const actionFetchRemoveAccount = (accountName: string) => async (
     dispatch: Dispatch,
     getState: () => IRootState,
@@ -201,12 +216,10 @@ export const actionFetchRemoveAccount = (accountName: string) => async (
         if (defaultAccountName === accountName) {
             const accounts = wallet.masterAccount.getAccounts();
             const account = accounts.find((item) => item.name !== accountName);
-
             if (account) {
                 await actionSwitchAccount(account.name);
             }
         }
-
         await wallet.masterAccount.removeAccount(accountName);
         await dispatch(actionUpdateWallet(wallet));
         await actionSaveWallet()(dispatch, getState);
