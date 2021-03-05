d5c6790f2805b05c30e4545205d710f5c47d973d
duy.nvt <duy.n@incognito.org>
2021-03-05 11:54:09 +0700

    lost connect screen dark mode

--- src/App.styled.ts
+++ src/App.styled.ts
@@ -1,6 +1,11 @@
 import styled from 'styled-components';
+import { ITheme } from 'src/styles';
 
 export const Styled = styled.div`
     .preload-container {
+    }
+    .lost-network {
+        background-color: ${(props: { theme: ITheme }) => props.theme.body};
+        color: ${(props: { theme: ITheme }) => props.theme.text};
     }
 `;
